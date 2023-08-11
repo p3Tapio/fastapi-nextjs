d892e928f4d01bdcf593c9779f89c4cc9735973b
Petri Laine <petri.laine@almamedia.fi>
2023-08-11 17:39:42 +0300

    Add more print

--- backend/config.py
+++ backend/config.py
@@ -7,10 +7,13 @@ secret_key = os.getenv("SECRET_KEY")
 algorithm = os.getenv("ALGORITH")
 environment = os.getenv("ENV", "development")
 
+
 env_variables = {
     "secret_key": secret_key,
     "algorithm": algorithm,
     "environment": environment,
 }
+
+print("ENV VARS: ", env_variables["algorithm"])
 
 origins = ["http://localhost:3001"]
