eb16de17c9c4a07bfc9364e16469f0b3979397c0
Petri Laine <petri.laine@almamedia.fi>
2023-09-29 15:24:40 +0300

    Install next, edit tsconfig, add base for frontend

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,5 @@
+// import variables from "..style/variables.modules.scss";
+
+export default function Page() {
+  return <h1>Hello, about page!</h1>
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,6 @@
+import variables from "./style/variables.modules.scss";
+import './style/base-style.scss'
+
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>;
+}
