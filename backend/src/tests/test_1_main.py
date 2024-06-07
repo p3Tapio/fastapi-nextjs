1562fa95e80468d9ab0e3872b7f66a56398e35b9
Petri Laine <petri.laine@almamedia.fi>
2024-06-07 14:46:06 +0300

    Print Sqlite version

--- backend/src/tests/test_1_main.py
+++ backend/src/tests/test_1_main.py
@@ -1,13 +1,12 @@
 from src.tests.utils import client
 import sqlite3
 
-print("SQLITE: " + sqlite3.sqlite_version)
-
 
 # TOOD -- clear db after tests
 
 
 def test_main():
     response = client.get("/")
+    print("SQLITE: " + sqlite3.sqlite_version)
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, I'm alive!"}
